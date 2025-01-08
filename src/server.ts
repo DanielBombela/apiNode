7069650412c279db8fe5f9a9a91c5e9455e958b7
DanielBombela <jdbombela@gmail.com>
2025-01-08 00:45:14 -0600

    mvc

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,112 @@
-/**
- * 
-const app = express();
-const PORT = process.env.PORT || "3000";
 
-app.get('/',(req,res)=>{
-   
-    res.json({message:"hola a todos"})
-})
-
-
-const main = async ()=>{
-    try {
-        await sequelize.sync({alter:true});
-        console.log("database connected")
-        app.listen(PORT,()=>{
-            console.log("SERVER IS RUNNING ON http://localhost:" + PORT )
-        })
-    } catch (error) {
-        console.log(error);
-        
-    }
-
-}
-main();
-
- */
-
-
+import route from "./routes";
+import http from "http";
 import express, { Application } from 'express';
 import { sequelize } from './database/sequelize';
 import 'dotenv/config'
-
+import cors from "cors";
+import morgan from "morgan";
 class Server{
     private app: Application;
     private port:string;
+    private static _intance: Server;
+    private Server: http.Server;
 
     constructor(){
         this.app = express();
-
+        this.middlewares();
+        this.routes();
+       // this.ConectionDB();
         this.port = process.env.PORT || "3000";
+        this.Server = new http.Server(this.app);
     }
 
     async listen(){
         try {
-            console.log(process.env.PORT)
+            this.Server.listen(this.port, () => {
+                console.log("Servidor corriendo en puerto " + this.port);
+              });
+           /**
+            *  console.log(process.env.PORT)
             await sequelize.sync({alter:true});
             console.log("database connected")
             this.app.listen(this.port,() => {
                 console.log('SERVIDOR CORRIENDO EN PUERTO ' + this.port);
                 
             })
+            */
         } catch (error) {
             console.log(error);
         }
      
     }
+
+    public static get instance() {
+        return this._intance || (this._intance = new this());
+      }
+
+      routes() {
+        this.app.use(`/api/v1/cards`, route);
+    
+        this.app.use((req, res, next) => {
+          const err: any = new Error("Not Found");
+          err.status = 404;
+          next(err);
+        });
+    
+        this.app.use((err, req, res, next) => {
+          console.log(err.stack);
+    
+          res.status(err.status || 500);
+          res.json({
+            statusCode: err.status || 500,
+            errors: {
+              message: err.message,
+              error: err,
+            },
+          });
+        });
+      }
+     
+      middlewares() {
+        this.app.use(cors()); //
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(express.json());
+        this.app.use(morgan("dev"));
+      }
+  
+      /**
+       *  async dbConection(out: boolean) {
+        try {
+          if (out) {
+            return true;
+          } else {
+            await db.authenticate();
+            console.log("Database is connect!");
+            return true;
+          }
+        } catch (error: any) {
+          console.error(error);
+          // db.close();
+          setTimeout(() => {
+            return this.dbConection(false);
+          }, 5000);
+          //  throw new Error(error);
+        }
+      }
+
+        
+      async ConectionDB() {
+        (async () => {
+          try {
+            await this.dbConection(false);
+          } catch (error) {
+            console.error(error);
+            throw new Error(error);
+          }
+        })();
+      }
+       */
 }
 
 
